5c4c49f8c314bde2cd89f817bfc677303dc1edb0
oscarsaleta <oscar.saleta@gmail.com>
2015-08-05 19:34:32 +0200

    Add new sets

--- R/MLE/alg.R
+++ R/MLE/alg.R
@@ -1,16 +1,19 @@
 library("evir")
 
-
 # Llegir dades
 g=read.table("/home/slenderman/git/tdg-mates/R/Power law/g-terrorism.txt")$V1
+#metode1
 P=ecdf(g)
 data=P(g)
 plot(data~g,log="yx")
-
+#metode2
 g.freqs=as.data.frame(table(g))
 g.freqs$Relative=as.numeric(prop.table(table(g)))
 g.freqs$g=as.numeric(g.freqs$g)
 plot(Relative~g,data=g.freqs,log="xy")
+#metode3
+data2=1+1e-4-P(g)
+plot(data2~g,log="xy")
 
 hist(g,probability = TRUE,breaks="FD")
 lines(density(g))
--- R/MLE/g-test.R
+++ R/MLE/g-test.R
@@ -1,18 +1,8 @@
 library(stats);
 library(evir);
 
-g=read.table("/home/slenderman/git/tdg-mates/R/Power law/g-terrorism.txt");
-g=g$V1;
-g=sort(g,decreasing=TRUE)
+x=read.table("/home/slenderman/git/tdg-mates/R/Power law/g-terrorism.txt")$V1;
+P=ecdf(x);
+data=1+1e-4-P(x);
+plot(data~x,log="xy")
 
-plot(g)
-hist(g[g>50 & g<2000],breaks=length(g[g>50 & g<2000]))
-plot(log(g[g>50 & g<2000]),log="xy")
-
-
-freq=seq(1/length(g),1,by=1/length(g))
-plot(log(g),log(10*freq),log="xy")
-
-log(g)
-log(10*freq)
-freq
