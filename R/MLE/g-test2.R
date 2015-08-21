2ac931b2f2c9d442a45493925f8d9c4d99374a19
Oscar Saleta <oscar.saleta@gmail.com>
2015-08-21 18:40:10 +0200

    Mes PL vs GPD. Falten GOF tests

--- R/MLE/g-test2.R
+++ R/MLE/g-test2.R
@@ -3,20 +3,21 @@ source("MLE/[34]eGPD.R");
 x=read.table("Power law/g-terrorism.txt")$V1;
 x=sort(x);
 xmin=12;
+alpha=2.4;
 x.excess=x[x>xmin]-xmin;
 
 fit=eGPD(x.excess)
 
 x.ecdf=1-ecdf(x.excess)(x.excess)
 x.fgpd=1-FGPD(x.excess,fit$k,fit$psi)
-x.pl=1-PL(x,12,2.4)
+x.pl=1-PL(x,xmin,alpha)
 
 pdf("g-PLvsGPD.pdf",width=10,height=8)
 
-plot(x.excess,x.ecdf,log="xy",xlab="Severitat d'atacs terroristes (morts)",ylab="CDF dades",
+plot(x.excess+xmin,x.ecdf,log="xy",xlab="Severitat d'atacs terroristes (morts)",ylab="CDF dades",
      panel.first=grid(equilogs=FALSE))
-lines(x.excess,x.fgpd,col="red")
-lines(x-12,x.pl,col="blue")
+lines(x.excess+xmin,x.fgpd,col="red")
+lines(x,x.pl,col="blue")
 legend("bottomright",c("Power-Law","GPD"),col=c("blue","red"),lty=c(1,1),inset = 0.02)
 
 dev.off()
--- R/MLE/k-test2.R
+++ R/MLE/k-test2.R
@@ -1,9 +1,9 @@
 source("MLE/[34]eGPD.R");
 
-x=read.table("Power law/a-words.txt")$V1;
+x=read.table("Power law/k-blackouts.txt")$V1;
 x=sort(x);
-xmin=7;
-alpha=1.95;
+xmin=230;
+alpha=2.3;
 x.excess=x[x>xmin]-xmin;
 
 fit=eGPD(x.excess)
@@ -12,13 +12,13 @@ x.ecdf=1-ecdf(x.excess)(x.excess)
 x.fgpd=1-FGPD(x.excess,fit$k,fit$psi)
 x.pl=1-PL(x,xmin,alpha)
 
-pdf("a-PLvsGPD.pdf",width=10,height=8)
+pdf("k-PLvsGPD.pdf",width=10,height=8)
 
-plot(x.excess+xmin,x.ecdf,log="xy",xlab="Freqüència d'ocurrència de paraules",ylab="CDF dades",
+plot(x.excess+xmin,x.ecdf,log="xy",xlab="Severitat d'atacs terroristes (morts)",ylab="CDF dades",
      panel.first=grid(equilogs=FALSE))
 lines(x.excess+xmin,x.fgpd,col="red")
 lines(x,x.pl,col="blue")
-legend("bottomright",c("Power-Law","GPD"),col=c("blue","red"),lty=c(1,1),inset = 0.02)
+legend("bottomleft",c("Power-Law","GPD"),col=c("blue","red"),lty=c(1,1),inset = 0.02)
 
 dev.off()
 
