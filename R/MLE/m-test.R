715fb9dfd27d173188a63d1159e967f70c895d6d
Oscar Saleta <oscar.saleta@gmail.com>
2015-08-28 23:58:16 +0200

    LAST COMMIT GOOD BYE

--- R/MLE/m-test.R
+++ R/MLE/m-test.R
@@ -12,7 +12,7 @@ legend("topright",c("Ajust GPD"),lty=1,inset = 0.01)
 dev.off()
 
 pdf("m-kk.pdf",width=10,height = 7)
-shape(x,models=30,start=15,end=350,labels=FALSE,reverse=TRUE)
+shape(x,models=30,start=15,end=750,labels=FALSE,reverse=TRUE)
 mtext(side=1,text="Dades sobre el llindar",line=2.5)
 mtext(side=3,text="Llindars",line=2.5)
 mtext(side=2,text=expression("Paràmetre de forma"~kappa~"(CI 95%)"),line=2.3)
